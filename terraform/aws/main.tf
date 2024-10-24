29c47758da299acf72d84e41506411764ef660fc
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-23 21:48:05 -0300

    push 2

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -1,178 +1,172 @@
-resource "aws_vpc" "vpc" {
+# VPC
+resource "aws_vpc" "vpc10" {
     cidr_block           = "10.0.0.0/16"
     enable_dns_hostnames = "true"
+    tags = {
+        Name = "vpc10"
+    }
 }
 
-resource "aws_internet_gateway" "igw" {
-    vpc_id = aws_vpc.vpc.id
+resource "aws_vpc" "vpc20" {
+    cidr_block           = "20.0.0.0/16"
+    enable_dns_hostnames = "true"
+    tags = {
+        Name = "vpc20"
+    }
 }
 
-resource "aws_subnet" "subnet1a" {
-    vpc_id                  = aws_vpc.vpc.id
-    cidr_block              = "10.0.5.0/24"
-    map_public_ip_on_launch = "true"
+# SUBNETS
+resource "aws_subnet" "sn_vpc10_pub" {
+    vpc_id                  = aws_vpc.vpc10.id
+    cidr_block              = "10.0.1.0/24"
     availability_zone       = "us-east-1a"
+    map_public_ip_on_launch = true
+    tags = {
+        Name = "sn_vpc10"
+    }
 }
 
-resource "aws_subnet" "subnet1c" {
-    vpc_id                  = aws_vpc.vpc.id
-    cidr_block              = "10.0.6.0/24"
-    map_public_ip_on_launch = "true"
-    availability_zone       = "us-east-1c"
+resource "aws_subnet" "sn_vpc20_priv" {
+    vpc_id            = aws_vpc.vpc20.id
+    cidr_block        = "20.0.1.0/24"
+    availability_zone = "us-east-1a"
+    tags = {
+        Name = "sn_vpc20"
+    }
 }
 
-resource "aws_route_table" "route" {
-    vpc_id = aws_vpc.vpc.id
+# VPC PEERING
+resource "aws_vpc_peering_connection" "vpc_peering" {
+    peer_vpc_id   = aws_vpc.vpc20.id
+    vpc_id        = aws_vpc.vpc10.id
+    auto_accept   = true  
+    tags = {
+        Name = "vpc_peering"
+    }
+}
+
+# INTERNET GATEWAY
+resource "aws_internet_gateway" "igw_vpc10" {
+    vpc_id = aws_vpc.vpc10.id
+    tags = {
+        Name = "igw_vpc10"
+    }
+}
+
+# ROUTE TABLE
+resource "aws_route_table" "rt_sn_vpc10_pub" {
+    vpc_id = aws_vpc.vpc10.id
+    route {
+        cidr_block = "20.0.0.0/16"
+        gateway_id = aws_vpc_peering_connection.vpc_peering.id
+    }
     route {
         cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.igw.id
+        gateway_id = aws_internet_gateway.igw_vpc10.id
+    }
+    tags = {
+        Name = "rt_sn_vpc10_pub"
     }
 }
 
-resource "aws_route_table_association" "route_association1a" {
-    subnet_id      = aws_subnet.subnet1a.id
-    route_table_id = aws_route_table.route.id
+resource "aws_route_table" "rt_sn_vpc20_priv" {
+    vpc_id = aws_vpc.vpc20.id
+    route {
+        cidr_block = "10.0.0.0/16"
+        gateway_id = aws_vpc_peering_connection.vpc_peering.id
+    }
+    tags = {
+        Name = "rt_sn_vpc20_priv"
+    }
 }
 
-resource "aws_route_table_association" "route_association1c" {
-    subnet_id      = aws_subnet.subnet1c.id
-    route_table_id = aws_route_table.route.id
+# SUBNET ASSOCIATION
+resource "aws_route_table_association" "rt_sn_vpc10_pub_To_sn_vpc10_pub" {
+  subnet_id      = aws_subnet.sn_vpc10_pub.id
+  route_table_id = aws_route_table.rt_sn_vpc10_pub.id
 }
 
-resource "aws_security_group" "sglb" {
-    vpc_id = aws_vpc.vpc.id
+resource "aws_route_table_association" "rt_sn_vpc20_priv_To_sn_vpc20_priv" {
+  subnet_id      = aws_subnet.sn_vpc20_priv.id
+  route_table_id = aws_route_table.rt_sn_vpc20_priv.id
+}
+
+# SECURITY GROUPS
+resource "aws_security_group" "sg_ec2_vpc10_pub" {
+    vpc_id = aws_vpc.vpc10.id
     egress {
-        from_port   = 0
-        to_port     = 0
+        from_port   = "0"
+        to_port     = "0"
         protocol    = "-1"
         cidr_blocks = ["0.0.0.0/0"]
     }
     ingress {
-        from_port   = 0
-        to_port     = 0
+        from_port   = "0"
+        to_port     = "0"
         protocol    = "-1"
         cidr_blocks = ["10.0.0.0/16"]
     }
     ingress {
-        description = "TCP/80 from All"
-        from_port   = 80
-        to_port     = 80
+        from_port   = "0"
+        to_port     = "0"
+        protocol    = "-1"
+        cidr_blocks = ["20.0.0.0/16"]
+    }
+    ingress {
+        from_port   = "3389"
+        to_port     = "3389"
         protocol    = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
+    tags = {
+        Name = "sg_ec2_vpc10_pub"
+    }
 }
 
-resource "aws_security_group" "sgec2" {
-    vpc_id = aws_vpc.vpc.id
+resource "aws_security_group" "sg_ec2_vpc20_priv" {
+    vpc_id = aws_vpc.vpc20.id
     egress {
-        from_port   = 0
-        to_port     = 0
+        from_port   = "0"
+        to_port     = "0"
         protocol    = "-1"
         cidr_blocks = ["0.0.0.0/0"]
     }
     ingress {
-        from_port   = 0
-        to_port     = 0
+        from_port   = "0"
+        to_port     = "0"
         protocol    = "-1"
         cidr_blocks = ["10.0.0.0/16"]
     }
     ingress {
-        description = "TCP/80 from All"
-        from_port   = 80
-        to_port     = 80
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
+        from_port   = "0"
+        to_port     = "0"
+        protocol    = "-1"
+        cidr_blocks = ["20.0.0.0/16"]
     }
-    ingress {
-        description = "TCP/22 from All"
-        from_port   = 22
-        to_port     = 22
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
+    tags = {
+        Name = "sg_ec2_vpc20_priv"
     }
 }
 
-resource "aws_instance" "instance01" {
-    ami                    = "ami-0f409bae3775dc8e5"
+# EC2 INSTANCES
+resource "aws_instance" "instance_sn_vpc10_pub" {
+    ami                    = "ami-0e38fa17744b2f6a5"
     instance_type          = "t2.micro"
-    subnet_id              = aws_subnet.subnet1a.id
-    vpc_security_group_ids = [aws_security_group.sgec2.id]
-    user_data              = <<EOF
-        #!/bin/bash
-        yum update -y
-        yum install -y httpd
-        echo "staticsite-lb-multi-cloud - AWS - instance01" > /var/www/html/index.html
-        service httpd restart
-    EOF
-}
-
-resource "aws_instance" "instance02" {
-    ami                    = "ami-0f409bae3775dc8e5"
-    instance_type          = "t2.micro"
-    subnet_id              = aws_subnet.subnet1a.id
-    vpc_security_group_ids = [aws_security_group.sgec2.id]
-    user_data              = <<EOF
-        #!/bin/bash
-        yum update -y
-        yum install -y httpd
-        echo "staticsite-lb-multi-cloud - AWS - instance02" > /var/www/html/index.html
-        service httpd restart
-    EOF
-}
-
-resource "aws_instance" "instance03" {
-    ami                    = "ami-0f409bae3775dc8e5"
-    instance_type          = "t2.micro"
-    subnet_id              = aws_subnet.subnet1c.id
-    vpc_security_group_ids = [aws_security_group.sgec2.id]
-    user_data              = <<EOF
-        #!/bin/bash
-        yum update -y
-        yum install -y httpd
-        echo "staticsite-lb-multi-cloud - AWS - instance03" > /var/www/html/index.html
-        service httpd restart
-    EOF
-}
-
-resource "aws_instance" "instance04" {
-    ami                    = "ami-0f409bae3775dc8e5"
-    instance_type          = "t2.micro"
-    subnet_id              = aws_subnet.subnet1c.id
-    vpc_security_group_ids = [aws_security_group.sgec2.id]
-    user_data              = <<EOF
-        #!/bin/bash
-        yum update -y
-        yum install -y httpd
-        echo "staticsite-lb-multi-cloud - AWS - instance04" > /var/www/html/index.html
-        service httpd restart
-    EOF
-}
-
-resource "aws_elb" "elb" {
-    name            = "staticsite-lb-aws-robelio"
-    security_groups = [aws_security_group.sglb.id]
-    subnets         = [aws_subnet.subnet1a.id, aws_subnet.subnet1c.id]
-    listener {
-        instance_port     = 80
-        instance_protocol = "http"
-        lb_port           = 80
-        lb_protocol       = "http"
+    subnet_id              = aws_subnet.sn_vpc10_pub.id
+    vpc_security_group_ids = [aws_security_group.sg_ec2_vpc10_pub.id]
+    key_name               = "vockey"
+    tags = {
+        Name = "instance_sn_vpc10_pub"
     }
-    health_check {
-        healthy_threshold   = 2
-        unhealthy_threshold = 2
-        timeout             = 3
-        target              = "HTTP:80/"
-        interval            = 30
-    }
-    instances = [
-        aws_instance.instance01.id, 
-        aws_instance.instance02.id,
-        aws_instance.instance03.id,
-        aws_instance.instance04.id
-    ]
 }
 
-output "elb_dns_name" {
-    value = aws_elb.elb.dns_name
+resource "aws_instance" "instance_sn_vpc20_priv" {
+    ami                    = "ami-0e38fa17744b2f6a5"
+    instance_type          = "t2.micro"
+    subnet_id              = aws_subnet.sn_vpc20_priv.id
+    vpc_security_group_ids = [aws_security_group.sg_ec2_vpc20_priv.id]
+    key_name               = "vockey"
+    tags = {
+        Name = "instance_sn_vpc20_priv"
+    }
 }
